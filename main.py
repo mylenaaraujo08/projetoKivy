906515376e2a43c9c9084ae88ae389769573ff6c
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-01-25 10:03:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,13 @@ class MyApp(App):
     def show_options(self, instance):
         # Página de Opções
         options_layout = BoxLayout(orientation='vertical', spacing=30, padding=20)
-        option1_button = Button(text='Opção Posterior', on_press=self.show_workouts_page, size_hint=(None, None))
-        option2_button = Button(text='Opção Inferior', on_press=self.show_workouts_page, size_hint=(None, None))
+        
+        # Adicionando o título "Escolha o Treino" no topo da tela
+        options_title = Label(text='Escolha o Treino', font_size=25, size_hint_y=None, height=50)
+        options_layout.add_widget(options_title)
+
+        option1_button = Button(text='Opção Inferior', on_press=lambda x: self.show_workouts_page(x, 'inferior'), size_hint=(None, None))
+        option2_button = Button(text='Opção Superior', on_press=lambda x: self.show_workouts_page(x, 'superior'), size_hint=(None, None))
         option1_button.size = option2_button.size = (500, 80)
         option1_button.pos_hint = {'center_x': 0.5}
         option2_button.pos_hint = {'center_x': 0.5}
@@ -44,86 +49,66 @@ class MyApp(App):
         # Atualizando o layout
         self.update_layout(options_layout)
 
-    def show_workouts_page(self, instance):
+    def show_workouts_page(self, instance, option):
         # Página de Treinos
         workouts_layout = BoxLayout(orientation='vertical', spacing=30, padding=20)
-        workout_a_button = Button(text='Treino A', on_press=self.show_exercises_page_a, size_hint=(None, None))
-        workout_b_button = Button(text='Treino B', on_press=self.show_exercises_page, size_hint=(None, None))
-        workout_c_button = Button(text='Treino C', on_press=self.show_exercises_page, size_hint=(None, None))
-        workout_d_button = Button(text='Treino D', on_press=self.show_exercises_page, size_hint=(None, None))
-        workout_a_button.size = workout_b_button.size = workout_c_button.size = workout_d_button.size = (500, 80)
-        workout_a_button.pos_hint = {'center_x': 0.5}
-        workout_b_button.pos_hint = {'center_x': 0.5}
-        workout_c_button.pos_hint = {'center_x': 0.5}
-        workout_d_button.pos_hint = {'center_x': 0.5}
-        workout_a_button.background_color = workout_b_button.background_color = workout_c_button.background_color = workout_d_button.background_color = (0, 0.5, 0, 1)
+        
+        # Adicionando o título "Lista de Exercícios para o Treino" no topo da tela
+        workouts_title = Label(text='Lista de Exercícios para o Treino', font_size=25, size_hint_y=None, height=50)
+        workouts_layout.add_widget(workouts_title)
+        
+        workouts_label = Label(text='Escolha o Treino', font_size=25)
 
-        workouts_layout.add_widget(workout_a_button)
-        workouts_layout.add_widget(workout_b_button)
-        workouts_layout.add_widget(workout_c_button)
-        workouts_layout.add_widget(workout_d_button)
+        # Adicionando botões para cada treino
+        workouts_buttons = []
+        for i in range(4):
+            button = Button(text=f'Treino {chr(65 + i)}', on_press=lambda x, treino=chr(65 + i): self.show_exercises_page(x, option, treino), size_hint=(None, None))
+            button.size = (500, 80)
+            button.pos_hint = {'center_x': 0.5}
+            button.background_color = (0, 0.5, 0, 1)
+            workouts_buttons.append(button)
+            workouts_layout.add_widget(button)
 
         # Atualizando o layout
         self.update_layout(workouts_layout)
 
-    def show_exercises_page_a(self, instance):
-        # Página de Exercícios para o Treino A
+    def show_exercises_page(self, instance, option, treino):
+        # Página de Exercícios
         exercises_layout = BoxLayout(orientation='vertical', spacing=30, padding=20)
-        exercises_label = Label(text='Lista de Exercícios para o Treino A (posterior de coxa)', font_size=25)
+        
+        # Adicionando o título "Lista de Exercícios para o Treino" no topo da tela
+        exercises_title = Label(text='Lista de Exercícios para o Treino', font_size=25, size_hint_y=None, height=50)
+        exercises_layout.add_widget(exercises_title)
 
-        # Lista de exercícios específicos para o Treino A
-        exercise_list = [
-            "1. Alongamento Curvado de Pé (3x12)",
-            "2. Cadeira flexora (3x15)",
-            "3. Elevação pélvica na máquina (3x15)",
-            "4. Cadeira abdutora (3x15)",
-            "5. Stiff (3x12)",
-            "6. Panturrilha na máquina (3x15)",
-            "7. Esteira (15 min)"
-        ]
+        exercises_label = Label(text=f'Lista de Exercícios para o Treino {treino} - Opção {option.upper()}', font_size=25)
 
-        # Adicionando rótulo e lista de exercícios
-        exercises_layout.add_widget(exercises_label)
+        # Mapeamento de exercícios para cada treino e opção
+        exercises_map = {
+            ('inferior', 'A'): ["Mobilidade Curvado de Pé (3x12)", "Cadeira flexora (3x15)", "Elevação pélvica na maquina ( 3x15)", "cadeira abdutora (3x15)", "stiff (3x12)", "Panturrilheira (3x15)", "Esteira (15 minutos)"],
+            ('inferior', 'B'): ["Mobilidade posterior segurando com a ponta do tênis (3x12)", "Flexora deitada (mesa) (3x15)", "Cadeira abdutora (3x15)", "Leg extensor (3x15)", "Panturrilheira (3x15)", "Esteira ou bicicleta (15 minutos)"],
+            ('inferior', 'C'): ["Mobilidade de tornozelo solo (3x15)", "Agachamento barra hexagonal", "Pernada com apoio ao banco (3x12)", "Leg extensor(3x15)", "Panturrilheira (3x15)", "Esteira ou Elíptico (15 minutos)"],
+            ('inferior', 'D'): ["Mobilidade posterior segurando com a ponta do tênis (3x12)", "Elevação pelve maquina (4x1)", "Cadeira adutora (3x15)", "Leg press (3x15)", "Leg 45 maquina (3x15)", "Abdominal infra maquina (3x12)", "Esteira ou Elíptico (15 minutos)"],
+            ('superior', 'A'): ["Mobilidade rotação de ombro com superband (3x12)", "Supino reto (3x15)", "Rosca direta ( 3x15)", "Remada curvada (3x15)", "Tríceps Pulley (3x12)", "Abdominal supra (3x15)", "Esteira (15 min)"],
+            ('superior', 'B'): ["Mobilidade escapular", "Supino reto articulado (3x12)", "Voador (3x15)", "Elevação lateral com halter (3x12)", "Triceps corda (+tríceps testa com corda) (3x10 cada uma)", "Triceps francês com halter", "Esteira ou escada ( 15 minutos)"],
+            ('superior', 'C'): ["Mobilidade ombro no banco (dinâmico ou isométrico) (3x12)", "Remada fechada (3x15)", "Rosca direta ( 3x15)", "Supino (3x12)", "Elevação frontal (3x15)", "Esteira( 15 minutos)"],
+            ('superior', 'D'): ["Mobilidade escapular(3x12)", "Puxador frontal (3x12)", "Crucifixo (3x12)", "Tríceps paralela (3x12)", "Rosca martelo (+ rosca direta) (3x10 cada uma)", "Abdominal máquina (3x12)", "Esteira (15 minutos)"]
+        }
 
-        for exercise in exercise_list:
+        # Adicionando os exercícios correspondentes ao treino e opção selecionados
+        for exercise in exercises_map[(option, treino)]:
             exercise_label = Label(text=exercise)
             exercises_layout.add_widget(exercise_label)
 
         # Definindo a cor do rótulo para verde escuro
         exercises_label.color = (0, 0.5, 0, 1)
+        
+        # Adicionando o título no topo da tela
+        top_layout = BoxLayout(orientation='vertical', spacing=10)
+        top_layout.add_widget(exercises_label)
+        exercises_layout.add_widget(top_layout)
 
         # Atualizando o layout
         self.update_layout(exercises_layout)
-
-    def show_exercises_page(self, instance):
-        # Página de Exercícios
-        exercises_layout = BoxLayout(orientation='vertical', spacing=30, padding=20)
-        exercises_label = Label(text='Lista de Exercícios para o Treino selecionado', font_size=25)
-
-        # Adicione aqui a lógica para mostrar a lista de exercícios do treino selecionado
-
-        exercises_layout.add_widget(exercises_label)
-
-        # Definindo a cor do rótulo para verde escuro
-        exercises_label.color = (0, 0.5, 0, 1)
-
-        # Adicionando botões com a mesma cor de fundo verde escuro
-        exercise_button1 = Button(text='Exercício 1', on_press=self.on_exercise_button_press, size_hint=(None, None))
-        exercise_button2 = Button(text='Exercício 2', on_press=self.on_exercise_button_press, size_hint=(None, None))
-        exercise_button1.size = exercise_button2.size = (500, 80)
-        exercise_button1.pos_hint = {'center_x': 0.5}
-        exercise_button2.pos_hint = {'center_x': 0.5}
-        exercise_button1.background_color = exercise_button2.background_color = (0, 0.5, 0, 1)
-
-        exercises_layout.add_widget(exercise_button1)
-        exercises_layout.add_widget(exercise_button2)
-
-        # Atualizando o layout
-        self.update_layout(exercises_layout)
-
-    def on_exercise_button_press(self, instance):
-        # Adicione a lógica que você deseja executar quando um botão de exercício for pressionado
-        print(f'Botão de exercício pressionado: {instance.text}')
 
     def update_layout(self, new_layout):
         # Removendo antigos widgets e adicionando novos à layout
